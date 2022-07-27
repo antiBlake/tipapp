e93c36b4697c8a55223f2c2055f4db503a63e7dc
antiBlake <76494602+antiBlake@users.noreply.github.com>
2022-07-27 22:02:45 +0100

    second

--- src/Tip.js
+++ src/Tip.js
@@ -36,20 +36,28 @@ const Tip = () => {
     },[formData])
     const handleSubmit= (e)=>{
         e.preventDefault()
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                bill:"",
+                custom:"",
+                numberpeople:"",
+            }
+        })
     }
 
     
   return (
     <div className='tip md:m-auto md:w-5/12'>
         <div className='tip-body h-auto w-12/12'>
-            <h2 className='text-center mt-20 text-4xl font-sans text-green-900 font-bold '>TIP APP</h2>
+            <h2 className='text-center mt-20 text-4xl font-sans text-teal-800 font-bold '>TIP APP</h2>
 
         </div>
         <div className='calculate w-12/12 mt-20 bg-white rounded-t-3xl p-6'>
             <div className='input-amount flex flex-col w-full'>
-                <h1 className='text-blue-300 mb-4 font-medium'>Bill</h1>
+                <h1 className='text-gray-400 mb-4 font-medium'>Bill</h1>
                 <div className='input '>
-                    <input type='number' className='border rounded-md px-4 w-full text-right h-10 bg-gray-100 text-2xl text-green-900'
+                    <input type='number' className='border rounded-md px-4 w-full text-right h-10 bg-gray-100 text-2xl text-teal-900'
                     name='bill'
                     onChange={handleChange}
                     value={formData.bill}
@@ -60,11 +68,13 @@ const Tip = () => {
 
             </div>
             <div className='percents-div flex flex-col w-full mt-12'>
-            <h1 className='text-blue-300 mb-4 font-medium'>Select Tip %</h1>
+            <h1 className='text-gray-400 mb-4 font-medium'>Select Tip %</h1>
             <div className='percent-box grid grid-cols-2 gap-2 justify-items-center'>
-                <div className='box-1 flex justify-center items-center bg-green-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-green-500 hover:text-green-900' onClick={()=>{
+                <div className='box-1 flex justify-center items-center bg-teal-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-teal-500 hover:text-teal-900' onClick={()=>{
         let calculateamount = (formData.bill * 5)/100
         setAmount(calculateamount)
+        let total = calculateamount * formData.numberpeople
+        setTotal(total)
         
 
         
@@ -73,18 +83,22 @@ const Tip = () => {
                     5%
 
                 </div>
-                <div className='box-1 flex justify-center items-center bg-green-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-green-500 hover:text-green-900' onClick={()=>{
+                <div className='box-1 flex justify-center items-center bg-teal-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-teal-500 hover:text-teal-900' onClick={()=>{
         let calculateamount = (formData.bill * 10)/100
         setAmount(calculateamount)
+        let total = calculateamount * formData.numberpeople
+        setTotal(total)
 
     }}>
                     10%
                     
                     </div>
 
-                    <div className='box-1 flex justify-center items-center bg-green-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-green-500 hover:text-green-900' onClick={()=>{
+                    <div className='box-1 flex justify-center items-center bg-teal-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-teal-500 hover:text-teal-900' onClick={()=>{
         let calculateamount = (formData.bill * 15)/100
         setAmount(calculateamount)
+        let total = calculateamount * formData.numberpeople
+        setTotal(total)
         
 
     }}>
@@ -92,17 +106,21 @@ const Tip = () => {
                     
                     </div>
 
-                    <div className='box-1 flex justify-center items-center bg-green-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-green-500 hover:text-green-900' onClick={()=>{
+                    <div className='box-1 flex justify-center items-center bg-teal-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-teal-500 hover:text-teal-900' onClick={()=>{
         let calculateamount = (formData.bill * 25)/100
         setAmount(calculateamount)
+        let total = calculateamount * formData.numberpeople
+        setTotal(total)
     }}>
                         25%
                     
                     </div>
 
-                    <div className='box-1 flex justify-center items-center bg-green-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-green-500 hover:text-green-900' onClick={()=>{
+                    <div className='box-1 flex justify-center items-center bg-teal-900 w-10/12 h-12 rounded-md text-white text-2xl font-medium hover:bg-teal-500 hover:text-teal-900' onClick={()=>{
         let calculateamount = (formData.bill * 50)/100
         setAmount(calculateamount)
+        let total = calculateamount * formData.numberpeople
+        setTotal(total)
 
     }}>
                         50%
@@ -110,7 +128,7 @@ const Tip = () => {
                     </div>
 
                     <div className='box-1 flex justify-center items-center  w-10/12 h-12 rounded-md text-white text-2xl font-medium'>
-                    <input type='number' className='border rounded-md px-4 w-full text-right h-12 bg-gray-100 text-2xl text-green-900 placeholder:text-green-900' placeholder='Custom'
+                    <input type='number' className='border rounded-md px-4 w-full text-right h-12 bg-gray-100 text-2xl text-teal-900 placeholder:text-teal-900' placeholder='Custom'
                     name='custom'
                     onChange={handleChange}
                     value={formData.custom}  />
@@ -121,9 +139,9 @@ const Tip = () => {
 
             </div>
             <div className='input-amount flex flex-col w-full my-8'>
-                <h1 className='text-blue-300 mb-4 font-medium'>Number of people</h1>
+                <h1 className='text-gray-400 mb-4 font-medium'>Number of people</h1>
                 <div className='input '>
-                    <input type='number' className='border rounded-md px-4 w-full text-right h-10 bg-gray-100 text-2xl text-green-900'
+                    <input type='number' className='border rounded-md px-4 w-full text-right h-10 bg-gray-100 text-2xl text-teal-900'
                     name='numberpeople'
                     onChange={handleChange}
                     value={formData.numberpeople}
@@ -133,7 +151,7 @@ const Tip = () => {
             </div>
 
             </div>
-            <div className='calculated w-full rounded-lg bg-green-900 text-white p-4 pt-8 grid grid-cols-1'>
+            <div className='calculated w-full rounded-lg bg-teal-900 text-white p-4 pt-8 grid grid-cols-1'>
                 <div className='tips flex justify-between'>
                     <div className='boxes'>
                         Tip Amount
@@ -158,7 +176,7 @@ const Tip = () => {
                         </div>
 
                 </div>
-                <button className='bg-green-500 text-green-900 rounded-md w-11/12 m-auto font-bold mt-8 h-12' onClick={handleSubmit}>RESET</button>
+                <button className='bg-teal-500 text-teal-900 rounded-md w-11/12 m-auto font-bold mt-8 h-12' onClick={handleSubmit}>RESET</button>
 
 
 
